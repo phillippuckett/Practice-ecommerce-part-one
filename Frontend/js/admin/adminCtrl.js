7c25440358f4bfb3070193ae3741e5753e02d160
J. Tanner Porter <jtporter9@gmail.com>
2015-11-17 19:29:46 -0700

    cool

--- Frontend/js/admin/adminCtrl.js
+++ Frontend/js/admin/adminCtrl.js
@@ -1,4 +1,22 @@
-angular.module('Ecommerce').controller('adminCtrl', function($scope, productList){
+angular.module('Ecommerce').controller('adminCtrl', function ($scope, productList) {
 	$scope.products = productList.data;
 	console.log($scope.products);
+
+	$scope.addProductBtn = function () {
+		$scope.addProduct = !$scope.addProduct;
+	};
+
+	$scope.submitNewProduct = function () {
+		$scope.addProduct = false;
+	};
+
+	$scope.editProductBtn = function () {
+		$scope.editProduct = !$scope.editProduct;
+	};
+	$scope.updateProductBtn = function () {
+		$scope.updateProduct = true;
+	};
+	$scope.submitUpdatedProduct = function () {
+		$scope.updateProduct = false;
+	};
 });
